777ad1fef6cf734f025e05c22eb634faa894ca65
Tzvi-H <16guitar@gmail.com>
2022-08-09 15:16:31 -0600

    passed phase 2

---
+++ practice-for-week-04-class-employee-manager-long-practice-main/manager.js
@@ -0,0 +1,14 @@
+const Employee = require("./employee");
+
+class Manager extends Employee {
+  constructor(name, salary, title, manager) {
+    super(name, salary, title, manager);
+    this.employees = [];
+  }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+}
+
+module.exports = Manager;
