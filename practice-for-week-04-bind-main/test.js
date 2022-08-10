e9ded0bac69211ae549f4e7fc5e87ef3e0033838
Tzvi-H <16guitar@gmail.com>
2022-08-10 16:27:40 -0600

    complete

---
+++ practice-for-week-04-bind-main/test.js
@@ -0,0 +1,6 @@
+const Employee = require("./employee");
+
+const john = new Employee("John Wick", "Dog Lover");
+
+setTimeout(john.sayName.bind(john), 2000);
+setTimeout(john.sayOccupation.bind(john), 3000);
