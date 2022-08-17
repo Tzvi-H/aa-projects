2ab612f5502266966fb71a1adb45489d04df68f3
Tzvi-H <16guitar@gmail.com>
2022-08-17 15:46:32 -0600

    Long Practice: TDD Style

--- practice-for-week-04-tdd-style-long-practice-main/problems/person.js
+++ practice-for-week-04-tdd-style-long-practice-main/problems/person.js
@@ -1,5 +1,44 @@
 class Person {
-  // Your code here
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    return `Hello, ${this.name}!`;
+  }
+
+  visit(otherPerson) {
+    return `${this.name} visited ${otherPerson.name}`;
+  }
+
+  switchVisit(otherPerson) {
+    return `${otherPerson.name} visited ${this.name}`;
+  }
+
+  update(obj) {
+    if (!(obj instanceof Person)) {
+      throw new TypeError("argument is not a valid Person object");
+    } else if (!obj.name || !obj.age) {
+      throw new TypeError("argument is missing age or name property");
+    }
+
+    this.name = obj.name;
+    this.age = obj.age;
+  }
+
+  tryUpdate(obj) {
+    try {
+      this.update(obj);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  static greetAll(persons) {
+    return persons.map((p) => p.sayHello());
+  }
 }
 
 module.exports = Person;
--- practice-for-week-04-tdd-style-long-practice-main/test/my-map-spec.js
+++ practice-for-week-04-tdd-style-long-practice-main/test/my-map-spec.js
@@ -1,1 +1,33 @@
-// Your code here
+const { expect } = require("chai");
+const chai = require("chai");
+const spies = require("chai-spies");
+chai.use(spies);
+
+const myMap = require("../problems/my-map");
+
+describe("myMap", () => {
+  let arr = [];
+  let callback;
+
+  beforeEach(() => {
+    arr = [1, 2, 3];
+    callback = (el) => el * 2;
+  });
+
+  it("invokes the function on each element in the array", () => {
+    const spy = chai.spy(callback);
+    expect(myMap(arr, spy)).to.eql([2, 4, 6]);
+    expect(spy).to.have.been.called.exactly(arr.length);
+  });
+
+  it("does not mutate the input array", () => {
+    myMap(arr, callback);
+    expect(arr).to.eql([1, 2, 3]);
+  });
+
+  it("does not invoke the built in Array.map method", () => {
+    const spy = chai.spy.on(arr, "map");
+    myMap(arr, callback);
+    expect(spy).not.to.have.been.called();
+  });
+});
