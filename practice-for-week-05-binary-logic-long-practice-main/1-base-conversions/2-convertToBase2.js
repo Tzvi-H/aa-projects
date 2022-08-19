e6132aad9e77960ef7f67bc55144cf93dac3d1cc
Tzvi-H <16guitar@gmail.com>
2022-08-19 16:30:06 -0600

    in middle of 2 (hexadecimal)

---
+++ practice-for-week-05-binary-logic-long-practice-main/1-base-conversions/2-convertToBase2.js
@@ -0,0 +1,43 @@
+// Convert the integers in the console.logs below to base 2
+
+/******************************************************************************/
+
+const convertToBase2 = (element) => {
+  let result = "";
+
+  if (typeof element === "number") {
+    let log2 = Math.floor(Math.log2(element));
+    for (let i = 0; i <= log2; i++) {
+      result += "0";
+    }
+    for (let i = 0; i < result.length; i++) {
+      let position = result.length - 1 - i;
+      let power = 2 ** position;
+
+      if (element / power >= 1) {
+        result = result.slice(0, i) + "1" + result.slice(i + 1);
+        element -= power;
+      }
+    }
+  }
+  return "0b" + result;
+};
+
+/******************************************************************************/
+
+console.log(convertToBase2(4) === "0b100"); //
+console.log(convertToBase2(65) === "0b1000001"); //
+console.log(convertToBase2(256) === "0b100000000"); //
+console.log(convertToBase2(123) === "0b1111011"); //
+console.log(convertToBase2(1000) === "0b1111101000"); //
+
+console.log("––––––");
+
+// console.log(convertToBase2("0xf")); // 0b1111
+// console.log(convertToBase2("0xfa")); // 0b11111010
+// console.log(convertToBase2("0x1234")); // 0b1001000110100
+// console.log(convertToBase2("0xc9a1")); // 0b1100100110100001
+// console.log(convertToBase2("0xbf12")); // 0b1011111100010010
+4;
+0;
+11;
