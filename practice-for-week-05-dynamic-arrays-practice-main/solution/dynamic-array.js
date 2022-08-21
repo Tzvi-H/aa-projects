cdbd5edaa13e03a2d7beaed4a96ed29fd356410b
Tzvi-H <16guitar@gmail.com>
2022-08-21 15:26:43 -0600

    Dynamic Arrays Practice

---
+++ practice-for-week-05-dynamic-arrays-practice-main/solution/dynamic-array.js
@@ -0,0 +1,21 @@
+class DynamicArray {
+  constructor(defaultSize = 4) {
+    this.data = new Array(defaultSize);
+    this.capacity = defaultSize;
+    this.length = 0;
+  }
+
+  read(index) {
+    return this.data[index];
+  }
+
+  unshift(val) {
+    for (let i = this.length - 1; i >= 0; i--) {
+      this.data[i + 1] = this.data[i];
+    }
+    this.data[0] = val;
+    this.length++;
+  }
+}
+
+module.exports = DynamicArray;
