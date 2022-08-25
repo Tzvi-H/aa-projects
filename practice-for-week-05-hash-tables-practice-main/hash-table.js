24c4e4ec8405f60d7ed42279738ecaeb46eb3480
Tzvi-H <16guitar@gmail.com>
2022-08-25 16:01:01 -0600

    Hash Table Practice Part 1

---
+++ practice-for-week-05-hash-tables-practice-main/hash-table.js
@@ -0,0 +1,41 @@
+const sha256 = require("js-sha256");
+
+class KeyValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class HashTable {
+  constructor(numBuckets = 4) {
+    this.data = new Array(numBuckets).fill(null);
+    this.count = 0;
+    this.capacity = numBuckets;
+  }
+
+  hash(key) {
+    const sha256Value = sha256(key);
+    const first8Chars = sha256Value.slice(0, 8);
+    return parseInt(first8Chars, 16);
+  }
+
+  hashMod(key) {
+    return this.hash(key) % this.capacity;
+  }
+
+  insertNoCollisions(key, value) {
+    // Your code here
+  }
+
+  insertWithHashCollisions(key, value) {
+    // Your code here
+  }
+
+  insert(key, value) {
+    // Your code here
+  }
+}
+
+module.exports = HashTable;
