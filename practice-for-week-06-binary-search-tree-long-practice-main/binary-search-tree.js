90747db1ca3a899fc3a78579bca5355072458d0a
Tzvi-H <16guitar@gmail.com>
2022-08-31 16:11:32 -0600

    Binary Trees Long Practice , in middle of deleteNodeBST

---
+++ practice-for-week-06-binary-search-tree-long-practice-main/binary-search-tree.js
@@ -0,0 +1,106 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, currentNode = this.root) {
+    const node = new TreeNode(val);
+    if (this.root === null) {
+      this.root = node;
+      return;
+    }
+
+    while (currentNode) {
+      if (val < currentNode.val) {
+        if (currentNode.left === null) {
+          currentNode.left = node;
+          return;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = node;
+          return;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  search(val) {
+    let currentNode = this.root;
+    while (currentNode) {
+      if (currentNode.val === val) {
+        return true;
+      } else if (val < currentNode.val) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
+  }
+
+  preOrderTraversal(currentNode = this.root) {
+    if (!currentNode) return;
+    console.log(currentNode.val);
+    this.preOrderTraversal(currentNode.left);
+    this.preOrderTraversal(currentNode.right);
+  }
+
+  inOrderTraversal(currentNode = this.root) {
+    if (!currentNode) return;
+    this.inOrderTraversal(currentNode.left);
+    console.log(currentNode.val);
+    this.inOrderTraversal(currentNode.right);
+  }
+
+  postOrderTraversal(currentNode = this.root) {
+    if (!currentNode) return;
+    this.postOrderTraversal(currentNode.left);
+    this.postOrderTraversal(currentNode.right);
+    console.log(currentNode.val);
+  }
+
+  // Breadth First Traversal - Iterative
+  breadthFirstTraversal() {
+    const queue = [this.root];
+    while (queue.length > 0) {
+      const front = queue.shift();
+      console.log(front.val);
+      if (front.left !== null) {
+        queue.push(front.left);
+      }
+      if (front.right !== null) {
+        queue.push(front.right);
+      }
+    }
+  }
+
+  // Depth First Traversal - Iterative
+  depthFirstTraversal() {
+    const queue = [this.root];
+    while (queue.length > 0) {
+      const front = queue.pop();
+      console.log(front.val);
+      if (front.left !== null) {
+        queue.push(front.left);
+      }
+      if (front.right !== null) {
+        queue.push(front.right);
+      }
+    }
+  }
+}
+
+module.exports = { TreeNode, BinarySearchTree };
---
+++ practice-for-week-06-binary-search-tree-long-practice-main/tree-practice.js
@@ -0,0 +1,154 @@
+const { BinarySearchTree, TreeNode } = require("./binary-search-tree.js");
+// Before starting, copy and paste your guided practice work into the copy
+// of `binary-search-tree.js` in this folder
+
+// Practice problems on binary trees
+
+function findMinBST(rootNode) {
+  let current = rootNode;
+  while (current && current.left) {
+    current = current.left;
+  }
+  return current.val;
+}
+
+function findMaxBST(rootNode) {
+  let current = rootNode;
+  while (current && current.right) {
+    current = current.right;
+  }
+  return current.val;
+}
+
+function findMinBT(rootNode) {
+  if (!rootNode) {
+    return Infinity;
+  }
+  const left = findMinBT(rootNode.left);
+  const right = findMinBT(rootNode.right);
+  return Math.min(rootNode.val, left, right);
+}
+
+function findMaxBT(rootNode) {
+  if (!rootNode) {
+    return -Infinity;
+  }
+  const left = findMaxBT(rootNode.left);
+  const right = findMaxBT(rootNode.right);
+  return Math.max(rootNode.val, left, right);
+}
+
+function getHeight(rootNode) {
+  if (!rootNode) {
+    return -1;
+  }
+  const leftHeight = getHeight(rootNode.left);
+  const rightHeight = getHeight(rootNode.right);
+
+  return 1 + Math.max(leftHeight, rightHeight);
+}
+
+function countNodes(rootNode) {
+  if (!rootNode) {
+    return 0;
+  }
+  return 1 + countNodes(rootNode.left) + countNodes(rootNode.right);
+}
+
+function balancedTree(rootNode) {
+  if (!rootNode.left && !rootNode.right) {
+    return true;
+  } else if (!rootNode.left || !rootNode.right) {
+    return false;
+  }
+
+  return balancedTree(rootNode.left) && balancedTree(rootNode.right);
+}
+
+function getParentNode(rootNode, target) {
+  if (rootNode.val === target) return null;
+
+  const queue = [rootNode];
+
+  while (queue.length !== 0) {
+    const current = queue.shift();
+
+    if (current.left && current.left.val === target) return current;
+    if (current.right && current.right.val === target) return current;
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+
+  return;
+}
+
+function inOrderPredecessor(rootNode, target) {
+  let values = [];
+
+  function inOrder(currentNode) {
+    if (!currentNode) return;
+    inOrder(currentNode.left);
+    values.push(currentNode.val);
+    inOrder(currentNode.right);
+  }
+
+  inOrder(rootNode);
+
+  for (let i = 0; i < values.length - 1; i++) {
+    if (values[i + 1] === target) {
+      return values[i];
+    }
+  }
+  return null;
+}
+
+function deleteNodeBST(rootNode, target) {
+  // Do a traversal to find the node. Keep track of the parent
+  // Undefined if the target cannot be found
+  // Set target based on parent
+  // Case 0: Zero children and no parent:
+  //   return null
+  // Case 1: Zero children:
+  //   set the parent that points to it to null
+  // Case 2: Two children:
+  //   set the value to its in-order predecessor, then delete the predecessor
+  // Case 3: One child:
+  //   Make the parent point to the child
+  const parent = getParentNode(rootNode, target);
+
+  if (!parent) return undefined;
+  let child;
+  let direction;
+  if (parent.left && parent.left.val === target) {
+    child = parent.left;
+    direction = "left";
+  } else {
+    child = parent.right;
+    direction = "right";
+  }
+
+  if (!child.left && !child.right) {
+    parent[direction] = null;
+  } else if (!child.left) {
+    parent[direction] = child.right;
+  } else if (!child.right) {
+    parent[direction] = child.left;
+  } else {
+    const predecessorValue = inOrderPredecessor(child, child.val);
+    deleteNodeBST(child, predecessorValue);
+    child.val = predecessorValue;
+  }
+}
+
+module.exports = {
+  findMinBST,
+  findMaxBST,
+  findMinBT,
+  findMaxBT,
+  getHeight,
+  countNodes,
+  balancedTree,
+  getParentNode,
+  inOrderPredecessor,
+  deleteNodeBST,
+};
