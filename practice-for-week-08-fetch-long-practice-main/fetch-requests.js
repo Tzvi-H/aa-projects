ecca5044c38f145d0ff0fb2d190cf941407ba25d
Tzvi-H <16guitar@gmail.com>
2022-09-22 15:15:30 -0600

    fetch Long Practice , phase 1

---
+++ practice-for-week-08-fetch-long-practice-main/fetch-requests.js
@@ -0,0 +1,22 @@
+/*
+Make fetch requests in the browser for each of the following phases.
+Paste your code for fetch requests here once you finish each phase.
+*/
+
+/* ============================== Phase 1 ============================== */
+
+fetch("/products", {
+  method: "POST",
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+});
+
+/* ============================== Phase 2 ============================== */
+
+// Your code here
+
+/* ============================== Phase 3 ============================== */
+
+// Your code here
