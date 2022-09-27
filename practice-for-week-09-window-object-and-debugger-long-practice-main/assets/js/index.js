036afda663d9a185be7d271e09ea59d0b4b4c9a7
Tzvi-H <16guitar@gmail.com>
2022-09-27 16:05:01 -0600

    Window Object and Debugger Long Practice : phase 2

--- practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/index.js
+++ practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/index.js
@@ -1,10 +1,13 @@
+import { changeAboutMe, changeHeader, changeTitle } from "./manipulate.js";
+import search from "./search.js";
+
 window.onload = function () {
   changeTitle();
   changeHeader();
   changeAboutMe();
-  console.log(findElementById("header")); // <div id="header">
-  console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
-  console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
-  console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
-  console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+  console.log(search.findElementById("header")); // <div id="header">
+  console.log(search.findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
+  console.log(search.findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
+  console.log(search.findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
+  console.log(search.findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
 };
