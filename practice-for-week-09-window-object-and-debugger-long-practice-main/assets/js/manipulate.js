ad4c52141eb87e182b76260c182e6ad511d00936
Tzvi-H <16guitar@gmail.com>
2022-09-28 15:36:48 -0600

    Window Object and Debugger Long Practice

--- practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/manipulate.js
+++ practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/manipulate.js
@@ -1,15 +1,15 @@
 export function changeTitle() {
-  // Change the title of the page to "(Your name)'s Portfolio"
-  // Your code here
+  document.title = "manipulated title";
 }
 
 export function changeHeader() {
   // Change the name in the h1 of the page to your name
-  // Your code here
+  document.querySelector("h1").innerText = "tzvi h";
 }
 
 export function changeAboutMe() {
   /* Update the first paragraph in the About Me section with a small
      passage about yourself */
-  // Your code here
+  document.querySelector("p").innerText =
+    "this paragraph has been edited with javascript";
 }
