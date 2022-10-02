cc5fc4f762f38c946ecb26625b62746bed583973
Tzvi-H <16guitar@gmail.com>
2022-10-02 16:55:48 -0600

    Practice - Express Route Handlers

---
+++ practice-for-week-10-express-route-handlers-main/server/app.js
@@ -0,0 +1,44 @@
+// Phase 2
+const {
+  getAllArtists,
+  getLatestArtist,
+  getArtistByArtistId,
+  addArtist,
+  editArtistByArtistId,
+  deleteArtistByArtistId,
+  getAlbumsForLatestArtist,
+  getAlbumsByArtistId,
+  getAlbumByAlbumId,
+  addAlbumByArtistId,
+  editAlbumByAlbumId,
+  deleteAlbumByAlbumId,
+  getFilteredAlbums,
+  getSongsByArtistId,
+  getSongsByAlbumId,
+  getSongBySongId,
+  addSongByAlbumId,
+  editSongBySongId,
+  deleteSongBySongId,
+} = require("./data");
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Body:", req.body);
+  next();
+});
+
+app.get("/artists", (req, res) => {
+  res.json(getAllArtists());
+});
+
+app.post("/artists", (req, res) => {
+  addArtist(req.body);
+  res.status(201).json(req.body);
+});
+
+const port = 5000;
+app.listen(port, () => console.log("Server is listening on port", port));
--- server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 5000;
-
-app.get("/status", (req, res) => {
-  res.send("The server is alive!");
-});
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
