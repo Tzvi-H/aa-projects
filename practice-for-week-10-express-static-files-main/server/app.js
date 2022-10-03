8ddc5c97186d757085afe6d44a35fd0ae9759f12
Tzvi-H <16guitar@gmail.com>
2022-10-03 12:52:37 -0600

    Bonus Practice - Serving Static Files in Express

--- practice-for-week-10-express-static-files-main/server/app.js
+++ practice-for-week-10-express-static-files-main/server/app.js
@@ -8,5 +8,7 @@ const app = express();
 
 app.use("/stylesheets", express.static("assets/css"));
 
+app.use("/stickers", express.static("assets/images"));
+
 const port = 5000;
 app.listen(port, () => console.log("Server is listening on port", port));
