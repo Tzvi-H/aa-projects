463c173342a8cd4cf27491ca2b410aa7f3c3fec4
Tzvi-H <16guitar@gmail.com>
2022-10-04 12:44:18 -0600

    Bonus Practice - Express Router

--- practice-for-week-10-express-router-main/server/routes/colors.js
+++ practice-for-week-10-express-router-main/server/routes/colors.js
@@ -10,4 +10,14 @@ router.get("/:name", (req, res) => {
   res.json(`GET /colors/${req.params.name}`);
 });
 
+router.post("/:name/css-styles", (req, res) => {
+  res.json(`POST /colors/${req.params.name}/css-styles`);
+});
+
+router.delete("/:name/css-styles/:style", (req, res) => {
+  res.json(
+    `DELETE /colors/${req.params.colors}/css-styles/${req.params.style}`
+  );
+});
+
 module.exports = router;
