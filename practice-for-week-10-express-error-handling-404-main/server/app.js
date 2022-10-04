8c99c4ed996bb8306bd88a609c0153f70582a43a
Tzvi-H <16guitar@gmail.com>
2022-10-04 12:24:48 -0600

    Practice - Error Handling Middleware

--- practice-for-week-10-express-error-handling-404-main/server/app.js
+++ practice-for-week-10-express-error-handling-404-main/server/app.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('GET / This is the root URL');
+app.get("/", (req, res) => {
+  res.send("GET / This is the root URL");
+});
+
+// undefined routes
+app.use((req, res, next) => {
+  const e = new Error("Sorry, the requested resource couldn't be found");
+  e.status = 404;
+  throw e;
+});
+
+// error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+
+  res.status(error.status || 500);
+  res.json({
+    message: error.message,
+    statusCode: error.status || 500,
+  });
 });
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
