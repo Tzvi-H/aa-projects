4fc1293cf7c1cfe32f82538c13f02e641f98f6a0
Tzvi-H <16guitar@gmail.com>
2022-10-07 13:09:40 -0600

    Practice CREATE/DROP Tables

---
+++ sql-practice/create-table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE puppies (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50),
+  age_yrs NUMERIC(3,1),
+  breed VARCHAR(100),
+  weight_lbs INTEGER,
+  microchipped BOOLEAN
+)
+
+-- id	integer		INTEGER PRIMARY KEY
+-- name	string	50	VARCHAR(50)
+-- age_yrs	number	3 digits, 1 decimal	NUMERIC(3,1)
+-- breed	string	100	VARCHAR(100)
+-- weight_lbs	number		INTEGER
+-- microchipped	Boolean		BOOLEAN
