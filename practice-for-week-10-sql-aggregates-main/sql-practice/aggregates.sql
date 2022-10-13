da6e00f151e3febfc8b9b619e8b23cd673df5e07
Tzvi-H <16guitar@gmail.com>
2022-10-13 13:03:41 -0600

    Bonus Practice SQL Aggregates

--- practice-for-week-10-sql-aggregates-main/sql-practice/aggregates.sql
+++ practice-for-week-10-sql-aggregates-main/sql-practice/aggregates.sql
@@ -4,14 +4,27 @@
 -- SELECT COUNT(id) AS count
 -- FROM cats;
 
-SELECT name, birth_year
-FROM cats
-GROUP BY birth_year
-ORDER BY birth_year
-LIMIT 1;
+-- SELECT name, birth_year
+-- FROM cats
+-- GROUP BY birth_year
+-- ORDER BY birth_year
+-- LIMIT 1;
 
-SELECT name, birth_year
+-- SELECT name, birth_year
+-- FROM cats
+-- GROUP BY birth_year
+-- ORDER BY birth_year DESC
+-- LIMIT 1;
+
+-- SELECT cats.name, COUNT(cats.id)
+-- FROM cats
+-- JOIN toys
+-- ON toys.cat_id = cats.id
+-- GROUP BY cats.name;
+
+SELECT cats.name, COUNT(cats.id) AS count
 FROM cats
-GROUP BY birth_year
-ORDER BY birth_year DESC
-LIMIT 1;
+JOIN toys
+ON toys.cat_id = cats.id
+GROUP BY cats.name
+HAVING count >= 2;
