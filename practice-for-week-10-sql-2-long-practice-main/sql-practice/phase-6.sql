c4994ab2e6531176da4bc0ef753ebc4dc858b1aa
Tzvi-H <16guitar@gmail.com>
2022-10-13 12:44:29 -0600

    Basic Phase 6 UPDATE

--- practice-for-week-10-sql-2-long-practice-main/sql-practice/phase-6.sql
+++ practice-for-week-10-sql-2-long-practice-main/sql-practice/phase-6.sql
@@ -1,2 +1,12 @@
 -- Update the toy with the name of "Cheetos" to have a name of "Pooky"
 -- Your code here
+.mode column
+.headers on
+
+SELECT name FROM toys;
+
+UPDATE toys
+SET name = 'Pooky'
+WHERE name = 'Cheetos';
+
+SELECT name FROM toys;
