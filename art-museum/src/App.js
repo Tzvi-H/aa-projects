4eafb83e34173ec59c47cc509ead5d50a57a281a
Tzvi-H <16guitar@gmail.com>
2022-10-14 17:21:08 -0600

    Art Museum pt.2

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,9 +1,17 @@
 import harvardArt from "./data/harvardArt";
-
 import GalleryNavigation from "./components/GalleryNavigation";
+import GalleryView from "./components/GalleryView";
+import { Route } from "react-router-dom";
 
 function App() {
-  return <GalleryNavigation galleries={harvardArt.records} />;
+  return (
+    <div className="page-wrapper">
+      <GalleryNavigation galleries={harvardArt.records} />
+      <Route path="/galleries/:galleryId">
+        <GalleryView galleries={harvardArt.records} />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
