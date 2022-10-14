212531e9b29be4b92fe9ab0bbe6a0e4d652de2c0
Tzvi-H <16guitar@gmail.com>
2022-10-14 17:25:46 -0600

    Art Museum pt.3

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,15 +1,35 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryView";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Harvard Art Museum</h1>
+      <p>
+        Look, but Don't Touch. Please select a Gallery in the navigation bar.
+      </p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="page-wrapper">
       <GalleryNavigation galleries={harvardArt.records} />
-      <Route path="/galleries/:galleryId">
-        <GalleryView galleries={harvardArt.records} />
-      </Route>
+
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/galleries/:galleryId">
+          <GalleryView galleries={harvardArt.records} />
+        </Route>
+        <Route>
+          <h2>Page Not Found</h2>
+        </Route>
+      </Switch>
     </div>
   );
 }
