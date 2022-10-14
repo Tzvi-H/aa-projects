86612a0344a510215a62bc9f666f169b0d8b0a57
Tzvi-H <16guitar@gmail.com>
2022-10-14 17:09:32 -0600

    Long Practice: Art Museum Project

---
+++ art-museum/src/App.js
@@ -0,0 +1,9 @@
+import harvardArt from "./data/harvardArt";
+
+import GalleryNavigation from "./components/GalleryNavigation";
+
+function App() {
+  return <GalleryNavigation galleries={harvardArt.records} />;
+}
+
+export default App;
