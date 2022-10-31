bffdbc701ea4f2e11efdf9d43472575f6c544516
Tzvi-H <16guitar@gmail.com>
2022-10-31 11:18:25 -0600

    https://my.appacademy.io/lessons/sequelize-efficiency/4657648b/practices/practice-efficient-querying-in-sequelize/935827c6

---
+++ practice-for-week-11-efficient-querying-in-sequelize-short-practice-main/server/db/migrations/20221031171552-add-index-to-books-price.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    queryInterface.addIndex("Books", ["price"]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    queryInterface.removeIndex("Books", ["price"]);
+  },
+};
